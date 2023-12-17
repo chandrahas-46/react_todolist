fbee2bc63c6f50d43b60a6d7ca1a5f46bb86fd38
Chandrahas Patel <70541620+chandrahas-46@users.noreply.github.com>
2023-12-17 14:36:36 +0530

    Add files via upload

---
+++ src/Component/TodoList.js
@@ -0,0 +1,225 @@
+import React, { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './style.css';
+import todoImage from "../image/todo.png"
+
+const TodoList = () => {
+  // State variables [{tasks: Task List}, {inputValue: Input field value}]
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [editTaskId, setEditTaskId] = useState(null);
+
+  // Fetch initial data
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  // Fetch todos from an API
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=6');
+      const todos = await response.json();
+      setTasks(todos);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  // Display loading message while data is being fetched
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // Handle input change
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // Add a new task
+  const handleAddTask = async () => {
+    if (inputValue.trim() === '') {
+      return;
+    }
+
+    const newTask = {
+      title: inputValue,
+      // id: new Date(),
+      completed: false
+    };
+
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        body: JSON.stringify(newTask),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      const addedTask = await response.json();
+      // console.log("add task: ", response);
+      setTasks((prevTasks) => [...prevTasks, addedTask]);
+      setInputValue('');
+      toast.success('Task added successfully');
+    } catch (error) {
+      console.log('Error adding task:', error);
+      toast.error('Error adding task');
+    }
+  };
+
+  // Handle checkbox change for a task
+  const handleTaskCheckboxChange = (taskId) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  // Edit a task
+  const handleEditTask = (taskId) => {
+    console.log("handleEditTask", taskId);
+    setEditTaskId(taskId);
+    const taskToEdit = tasks.find((task) => task.id === taskId);
+    setInputValue(taskToEdit.title);
+  };
+
+  // Delete a task
+  const handleDeleteTask = (taskId) => {
+    console.log("handleDeleteTask ", taskId);
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+    toast.success('Task deleted successfully');
+  };
+
+  // Update task
+  const handleUpdateTask = async () => {
+    console.log("handleUpdateTask", editTaskId);
+    if (inputValue.trim() === '') {
+      return;
+    }
+
+    const updatedTask = {
+      title: inputValue,
+      completed: false
+    };
+
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${editTaskId}`, {
+        method: 'PUT',
+        body: JSON.stringify(updatedTask),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      const updatedTaskData = await response.json();
+      setTasks((prevTasks) =>
+        prevTasks.map((task) =>
+          task.id === editTaskId ? { ...task, title: updatedTaskData.title } : task
+        )
+      );
+      setInputValue('');
+      setEditTaskId(null);
+      toast.success('Task updated successfully');
+    } catch (error) {
+      console.log('Error updating task:', error);
+      toast.error('Error updating task');
+    }
+  };
+
+  // Mark all tasks as completed
+  const handleCompleteAll = () => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) => ({ ...task, completed: true }))
+    );
+  };
+
+  // Clear completed tasks
+  const handleClearCompleted = () => {
+    setTasks((prevTasks) => prevTasks.filter((task) => !task.completed));
+  };
+
+
+  // Render the todo list
+  return (
+    <div className="container">
+      <ToastContainer />
+      <div className="todo-app">
+        <h2>
+          <img src={todoImage} alt="todo-image" /> Todo List
+        </h2>
+        <div className="row">
+          <i className="fas fa-list-check"></i>
+          <input
+            type="text"
+            className="add-task"
+            id="add"
+            placeholder="Add your todo"
+            autoFocus
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button id="btn" onClick={editTaskId ? handleUpdateTask : handleAddTask}>
+            {editTaskId ? 'Update' : 'Add'}
+          </button>
+        </div>
+
+        <div className="mid">
+          <i className="fas fa-check-double"></i>
+          <p id="complete-all" onClick={handleCompleteAll}>
+            Complete all tasks
+          </p>
+          <p id="clear-all" onClick={handleClearCompleted}>
+            Update/Delete tasks
+          </p>
+        </div>
+
+        <ul id="list">
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <input
+                type="checkbox"
+                id={`task-${task.id}`}
+                data-id={task.id}
+                className="custom-checkbox"
+                checked={task.completed}
+                onChange={() => handleTaskCheckboxChange(task.id)}
+              />
+              <label htmlFor={`task-${task.id}`}>{task.title}</label>
+              <div>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/1159/1159633.png"
+                  className="edit"
+                  data-id={task.id}
+                  onClick={() => handleEditTask(task.id)}
+                />
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/3096/3096673.png"
+                  className="delete"
+                  data-id={task.id}
+                  onClick={() => handleDeleteTask(task.id)}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        <div className='task-counter'>
+          <div>
+            <button>Completed: <span>{tasks.filter((task) => task.completed).length}</span></button>             
+          </div>
+          <div>
+            <button>Remaining: <span>{tasks.length - tasks.filter((task) => task.completed).length}</span></button>              
+          </div>
+          <div>
+            <button>Total Tasks: <span>{tasks.length}</span></button>             
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
